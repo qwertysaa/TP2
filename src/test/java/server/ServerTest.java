9c6227040f0ed6f86c85a45710acad912d8389b7
ImanBn <130778469+ImanBn@users.noreply.github.com>
2023-04-15 13:56:34 -0400

    Update ServerTest.java

--- src/test/java/server/ServerTest.java
+++ src/test/java/server/ServerTest.java
@@ -31,7 +31,8 @@ public class ServerTest extends TestCase {
             FileInputStream fileOs = new FileInputStream("listeCoursSessionSpecifiee.txt");
             ObjectInputStream os = new ObjectInputStream(fileOs);
 
-            ArrayList<Object> listecours = (ArrayList<Object>) os.readObject(); //ajouter objet dans fichier "sérialisé" pour le client
+            ArrayList<Object> listecours = (ArrayList<Object>) os.readObject(); //ajouter objet dans fichier
+            // "sérialisé" pour le client
             System.out.println(Arrays.asList(listecours)); //pour débogage
             for (Object o : listecours) {
                 System.out.println(Arrays.asList((String[]) o)); //pour débogage
@@ -58,7 +59,8 @@ public class ServerTest extends TestCase {
     @Test //expérimentation avec bufferedwriter
     public void testUn() {
         try {
-            FileWriter fw = new FileWriter("C:\\Users\\tokat\\IdeaProjects\\TP2\\src\\main\\java\\server\\data\\inscription.txt", true);
+            FileWriter fw = new FileWriter("C:\\Users\\tokat\\IdeaProjects\\TP2\\src\\main\\java\\server" +
+                    "\\data\\inscription.txt", true);
             BufferedWriter writer = new BufferedWriter(fw);
             String line = "ABC";
             writer.append(line);
@@ -89,7 +91,8 @@ public class ServerTest extends TestCase {
         FileInputStream fileOs = new FileInputStream("transferCourses.txt");
         ObjectInputStream os = new ObjectInputStream(fileOs);
 
-        ArrayList<Course> listecours = (ArrayList<Course>) os.readObject(); //ajouter objet dans fichier "sérialisé" pour le client
+        ArrayList<Course> listecours = (ArrayList<Course>) os.readObject(); //ajouter objet dans fichier "sérialisé"
+        // pour le client
         System.out.println(Arrays.asList(listecours)); //pour débogage
         for (Object o : listecours) {
             System.out.println(o.toString()); //pour débogage
