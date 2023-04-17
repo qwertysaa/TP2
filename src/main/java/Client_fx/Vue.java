e07e43d940f51e0fde991f6f9b6e0eae8802fa72
ImanBn <130778469+ImanBn@users.noreply.github.com>
2023-04-17 01:03:34 -0400

    Update Vue.java

--- src/main/java/Client_fx/Vue.java
+++ src/main/java/Client_fx/Vue.java
@@ -3,13 +3,13 @@ package Client_fx;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import server.models.Course;
 
@@ -31,16 +31,24 @@ public class Vue extends BorderPane {
 
     Vue() {
 
+        // Le VBox à gauche qui montre les cours disponibles
         VBox leftVBox = new VBox();
-        leftVBox.setSpacing(20); //Ajouter de l'espace
+        Scene scene = new Scene(leftVBox, 500, 500);
+
+        // Pour mettre couleur Beige au fond
+        leftVBox.setBackground(new Background(new BackgroundFill(Color.BEIGE, CornerRadii.EMPTY, Insets.EMPTY)));
         Text textCours = new Text("                          Liste des cours");
+
+        // Separer le leftVBox en 3 HBox, cours HBox est le premier
         HBox coursHBox = new HBox();
         coursHBox.getChildren().add(textCours);
         leftVBox.getChildren().add(coursHBox);
-        coursHBox.setPadding(new Insets(7));
+        coursHBox.setPadding(new Insets(5));
 
+        // pour mettre l'espace entre leftVBox avec le mur et le rightVBox
         leftVBox.setPadding(new Insets(10));
 
+        // Construire deux colonnes dans le Table qui représentent le code et le nom de cours disponibles
         leftVBox.getChildren().add(coursSession);
         coursSession.setEditable(true);
         TableColumn<Course, String> codeColumn = new TableColumn<>("Code");
@@ -50,17 +58,22 @@ public class Vue extends BorderPane {
         coursSession.getColumns().setAll(codeColumn,coursColumn);
         coursSession.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 
+        // Le deuxième HBox dans le leftVBox qui contient Table avec les cours
         HBox tableHBox = new HBox();
         tableHBox.getChildren().add(coursSession);
         leftVBox.getChildren().add(tableHBox);
 
-      /*  Separator sep = new Separator();
-        sep.setPrefHeight(25);
-        leftVBox.getChildren().add(sep);*/
+        // Separateur horizontale qui sépare le Table avec les deux boutons
+        Separator sepHoriz = new Separator();
+        sepHoriz.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+        leftVBox.getChildren().add(sepHoriz);
 
+        // Le dernier HBox dans le leftVBox qui contient boutons sessions et charger qui envoyent une demande au
+        // Serveur pour afficher les cours dispoibles dans la session désiré
         HBox bottomLeftHBox = new HBox();
         String[] sessions = {"Hiver", "Automne", "Ete"};
         session.getItems().addAll(sessions);
+
         HBox chargerHBox = new HBox();
         chargerHBox.getChildren().add(charger);
         chargerHBox.setAlignment(Pos.BASELINE_RIGHT);
@@ -69,53 +82,82 @@ public class Vue extends BorderPane {
         bottomLeftHBox.setHgrow(chargerHBox, Priority.ALWAYS);
         leftVBox.getChildren().add(bottomLeftHBox);
 
+        // Ajouter de l'espace entre le table et les boutons
+        leftVBox.setSpacing(20);
+
         this.setLeft(leftVBox);
 
+        // Construire le VBox à droite qui s'agit le formulaire d'inscription
         VBox rightVBox = new VBox();
+
+        Separator sepVert = new Separator();
+        sepVert.setOrientation(Orientation.VERTICAL);
+        sepVert.setPrefHeight(300);
+        sepVert.setPrefWidth(1);
+
+        // Pour mettre couleur Beige au fond
+        rightVBox.setBackground(new Background(new BackgroundFill(Color.BEIGE, CornerRadii.EMPTY, Insets.EMPTY)));
+        rightVBox.setPadding(new Insets(5));
+
+        // Le premier HBox dans le rightVBox qui contient le titre
         HBox inscription = new HBox();
         Text textInscription = new Text("                         Formulaire d'inscription");
         inscription.getChildren().add(textInscription);
         rightVBox.getChildren().add(inscription);
         inscription.setPadding(new Insets(10));
 
+        // Le deuxième HBox dans le rightVBox qui contient le champ prenom du formulaire
         HBox prenom = new HBox();
         Text textPrenom = new Text("Prénom    ");
         prenom.getChildren().add(textPrenom);
         prenom.getChildren().add(enterPrenom);
         rightVBox.getChildren().add(prenom);
-        enterPrenom.setMinWidth(200);
+        enterPrenom.setMinWidth(240);
         prenom.setPadding(new Insets(10));
 
-
+        // Le troisième HBox dans le rightVBox qui contient le champ nom du formulaire
         HBox nom = new HBox();
         Text textNom = new Text("Nom         ");
         nom.getChildren().add(textNom);
         nom.getChildren().add(enterNom);
         rightVBox.getChildren().add(nom);
-        nom.setPadding(new Insets(10));
-        enterNom.setMinWidth(200);
 
+        nom.setPadding(new Insets(10));
+        enterNom.setMinWidth(240);
+
+        // Le quatrième HBox dans le rightVBox qui contient le champ email du formulaire
         HBox email = new HBox();
         Text textEmail = new Text("Email        ");
         email.getChildren().add(textEmail);
         email.getChildren().add(enterEmail);
         rightVBox.getChildren().add(email);
-        email.setPadding(new Insets(10));
-        enterEmail.setMinWidth(200);
 
+        email.setPadding(new Insets(10));
+        enterEmail.setMinWidth(240);
+
+        // Le cinquième HBox dans le rightVBox qui contient le champ prenom du formulaire
         HBox matricule = new HBox();
         Text textMatricule = new Text("Matricule  ");
         matricule.getChildren().add(textMatricule);
         matricule.getChildren().add(enterMatricule);
         rightVBox.getChildren().add(matricule);
+
         matricule.setPadding(new Insets(10));
-        enterMatricule.setMinWidth(200);
+        enterMatricule.setMinWidth(240);
 
-        rightVBox.getChildren().add(envoyer);
+        // Le dernier HBox dans le rightVBox qui contient le bouton envoyer, ce dernier verifie si les informations
+        // sont rentrés correctement et ensuite update la fiche d'inscription et envoyer en message
+        HBox envoyerHBox = new HBox();
+        envoyerHBox.getChildren().add(envoyer);
+        rightVBox.getChildren().add(envoyerHBox);
+        
         envoyer.setPadding(new Insets(10));
+        envoyerHBox.setPadding(new Insets(10));
 
-        this.setCenter(rightVBox);
+        // Pour mettre l'espace blanc entre le rightVBox et le leftVBox
+        this.setRight(rightVBox);
     }
+
     public TableView<Course> getCoursSession() {
         return this.coursSession;
     }
