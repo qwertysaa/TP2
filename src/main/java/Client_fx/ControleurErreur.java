819910179b7139d376d38785291676f4d0cadace
ImanBn <130778469+ImanBn@users.noreply.github.com>
2023-04-17 16:30:24 -0400

    Create ControleurErreur.java

---
+++ src/main/java/Client_fx/ControleurErreur.java
@@ -0,0 +1,11 @@
+package Client_fx;
+public class ControleurErreur {
+
+    private Modele modele;
+    private Erreur erreur;
+
+    public ControleurErreur(Modele m, Erreur e) {
+        this.modele = m;
+        this.erreur = e;
+    }
+}
